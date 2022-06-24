d73ff444aad2daee892d90e35d785968b37767e0
Puglisi <pugliisi@gmail.com>
2022-06-24 01:34:52 +0000

    quote error

--- models/agg_carrier.sql
+++ models/agg_carrier.sql
@@ -13,6 +13,6 @@ avg(TIMESTAMP_DIFF(pickup_date, delivery_date, hour)) as avg_diff_hour,
 sum(TIMESTAMP_DIFF(delivery_date, pickup_date, hour)) as sum_diff_hour,
 sum(case when carrier_on_time_overall is true then 1 else 0 end) as ontime,
 sum(case when carrier_on_time_overall is false then 1 else 0 end) as not_ontime
-FROM `loadsmart-354023.bd_loadsmart.table_test` 
+FROM 'loadsmart-354023.bd_loadsmart.table_test'
 where carrier_name is not null
 group by carrier_name
