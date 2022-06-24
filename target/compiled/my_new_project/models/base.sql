3fb70a91103ec07a17c5ba92d2fdfcc31bfd7701
Nathalia Puglisi <pugliisi@gmail.com>
2022-06-23 22:12:12 -0300

    files out

--- target/compiled/my_new_project/models/base.sql
+++
@@ -1,4 +0,0 @@
-
-
-SELECT quote_date, equipment_type, carrier_name, pickup_date, pickup_appointment_time, delivery_date, delivery_appointment_time, mileage, book_price, TIMESTAMP_DIFF(delivery_date, pickup_date, hour) as diff_hour
-FROM 'loadsmart-354023.bd_loadsmart.table_test'
