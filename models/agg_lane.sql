78674140e6bd5380d2603ce5ae5163850acd3164
pugliisi@gmail.com <pugliisi@gmail.com>
2022-06-25 21:23:33 +0000

    fix name of column

--- models/agg_lane.sql
+++ models/agg_lane.sql
@@ -2,7 +2,7 @@
     materialized='table'
 )}}
 
-SELECT equipment_type, carrier_name, lane, avg(mileage) as avg_mile, avg(book_price) as avg_price, sum(price) as sum_price
+SELECT equipment_type, carrier_name, lane, avg(mileage) as avg_mile, avg(book_price) as avg_price, sum(book_price) as sum_price
 FROM loadsmart-354023.bd_loadsmart.table_test
 where carrier_name is not null
 group by equipment_type, carrier_name, lane
